950b6a85f090b36feb178f638d46168b916b5286
Vishnu S <vishnus510412@gmail.com>
2025-02-23 16:30:20 +0530

    feat: modules wise added room and home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import type { NextPage } from "next";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import Home from "@/modules/home/components/Home";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const HomePage: NextPage = () => {
+  return <Home />;
+};
 
-export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <div>Draw</div>
-    </div>
-  );
-}
+export default HomePage;
