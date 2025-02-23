8a6decf3580b7d3c47888764f78bf176ab513895
Vishnu S <vishnus510412@gmail.com>
2025-02-23 17:58:49 +0530

    old server removed

--- modules/home/components/Home.tsx
+++ modules/home/components/Home.tsx
@@ -56,7 +56,7 @@ const Home = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-6">
       <div className="w-full max-w-lg rounded-xl bg-white p-8 text-center shadow-lg">
-        <h1 className="text-4xl font-bold text-gray-800">Digiboard</h1>
+        <h1 className="text-4xl font-bold text-gray-800">Draw</h1>
         <p className="text-lg text-gray-500">
           Real-time collaborative whiteboard
         </p>
